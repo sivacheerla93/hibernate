ea89f497d3048192f8596c195fd0a4f68df45bfa
Siva Cheerla <sivacheerla@live.com>
2020-05-07 14:09:52 +0530

    07/05/20

---
+++ src/main/java/GetJob.java
@@ -0,0 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import entities.Job;
+
+public class GetJob {
+
+	public static void main(String[] args) throws Exception {
+		Configuration c = new Configuration();
+		c.configure(); // load hibernate.cfg.xml
+
+		SessionFactory sf = c.buildSessionFactory();
+		Session s = sf.openSession();
+
+		Job job = s.load(Job.class, "IT_PROG");
+		System.out.println(job.getClass());
+		System.out.println(job.getTitle());
+
+		s.close();
+		sf.close();
+	}
+}
